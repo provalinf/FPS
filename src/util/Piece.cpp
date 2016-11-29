b8faa321bb396f97f1cebe9fd1a9fc92eea984a1
Cyril <cyril_basile@hotmail.fr>
2016-11-29 18:06:14 +0100

    amélioration pieces

--- src/util/Piece.cpp
+++ src/util/Piece.cpp
@@ -12,12 +12,17 @@ Piece::Piece(Model *model) : Object(model) {
 
 void Piece::CreateCoin(int x, int y) {
 
-	//glColor3ub(157, 0, 157);
-	LoadTexture(0);
+	//LoadTexture(0);
+    glColor3ub(254, 236, 14);
 	GLUquadricObj *quadra = gluNewQuadric();
+
 	glPushMatrix();
 	glTranslatef(x, y, model->getHauteurPiece());
-	gluSphere(quadra, 0.3, 10, 10);
+    glRotatef(90,1,0,0);
+    glRotatef(clock(),0,1,0);
+    gluCylinder(quadra,0.2,0.2,0.1,80,1);
+    gluDisk(quadra,0,0.2,30,30);
+	//gluSphere(quadra, 0.3, 10, 10);
 	glPopMatrix();
 	gluDeleteQuadric(quadra);
 }
